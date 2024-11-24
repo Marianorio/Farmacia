7fae730b0d972c80c7fd1403380e54d171789a8c
Mariannorio <rodriguezmarianoarg@gmail.com>
2024-11-24 13:50:25 -0300

    se suben los cambios a ventas rama, se oculta el id

--- resources/views/ventas/ventas.blade.php
+++ resources/views/ventas/ventas.blade.php
@@ -24,7 +24,6 @@
             <table class="table table-striped" id="tablaPrincipal">
                 <thead>
                     <tr>
-                        <th>ID</th>
                         <th>Cliente</th>
                         <th>Fecha</th>
                         <th>Total</th>
@@ -35,7 +34,6 @@
                 <tbody>
                     @foreach($ventas as $venta)
                     <tr>
-                        <td>{{ $venta->id }}</td>
                         <td>{{ $venta->cliente->nombre }}</td>
                         <td>{{ $venta->fecha }}</td>
                         <td>${{ number_format($venta->total, 2) }}</td>
